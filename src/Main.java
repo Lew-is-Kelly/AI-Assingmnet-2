f14d2e1650d0dbc96790e7b735e8243a3cb970e3
Lewis Kelly <lewisepickelly@gmail.com>
2022-10-31 13:06:28 -0400

    Added canPlace and placeQueen functions

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,6 @@
 public class Main {
 
   /**
-   * Main function, solves 4 queens problem.
-   */
-  public static void main(String[] args) {
-    System.out.println("4 Queens Problem");
-
-    boolean[][] grid = makeGrid(4);
-    printGrid(grid);
-  }
-
-  /**
    * Creates a square 2D boolean array representing the chess board.
    *
    * @param gridSize Size of the grid to be made.
@@ -48,5 +38,93 @@ public class Main {
       }
       System.out.print("\n");
     }
+  }
+
+  /**
+   * Checks if a position is under attack from a queen.
+   *
+   * @param posX X coordinate of position to check.
+   * @param posY Y coordinate of position to check.
+   * @param grid Boolean grid of the board.
+   * @return true if the position is not under attack.
+   */
+  public static boolean canPlace (int posX, int posY, boolean[][] grid) {
+    // Check place provided if queen is present
+    if (grid[posX][posY]) {
+      return false;
+    }
+
+    for (int i = 1; i < grid.length; i++) {
+      int upX = posX + i;
+      int dnX = posX - i;
+      int upY = posY + i;
+      int dnY = posY - i;
+
+      //Check horizontal
+      if (upX <= grid.length - 1) {
+        if (grid[upX][posY]) {
+          return false;
+        }
+      }
+      if (dnX >= 0) {
+        if (grid[dnX][posY]) {
+          return false;
+        }
+      }
+
+      // Check vertical
+      if (upY <= grid.length - 1) {
+        if (grid[posX][upY]) {
+          return false;
+        }
+      }
+      if (dnY >= 0) {
+        if (grid[posX][dnY]) {
+          return false;
+        }
+      }
+
+      // Check diagonals
+      if (dnX >= 0 && dnY >= 0) {
+        if (grid[dnX][dnY]) {
+          return false;
+        }
+      }
+      if (upX <= grid.length - 1 && dnY >= 0) {
+        if (grid[upX][dnY]) {
+          return false;
+        }
+      }
+      if (dnX >= 0 && upY <= grid.length - 1) {
+        if (grid[dnX][upY]) {
+          return false;
+        }
+      }
+      if (upX <= grid.length - 1 && upY <= grid.length - 1) {
+        if (grid[upX][upY]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public static void placeQueen (int posX, int posY, boolean[][] grid) {
+    grid[posX][posY] = true;
+  }
+
+  /**
+   * Main.
+   */
+  public static void main(String[] args) {
+    System.out.println("4 Queens Problem");
+
+    boolean[][] grid = makeGrid(4);
+    placeQueen(0, 0, grid);
+
+    System.out.printf("%b\n", canPlace(3, 0, grid));
+
+    printGrid(grid);
   }
 }
